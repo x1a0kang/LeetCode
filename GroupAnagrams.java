f81ae7940d3c7bd7ab8f5eb3e1b19f7a80565787
x1a0kang <18810298505@163.com>
2025-03-02 16:30:10 +0800

    字母异位词分组

--- GroupAnagrams.java
+++ GroupAnagrams.java
@@ -2,19 +2,19 @@ import java.util.*;
 
 public class GroupAnagrams {
     public static void main(String[] args) {
-        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        String[] strList = {"eat", "tea", "tan", "ate", "nat", "bat"};
         List<List<String>> lists = groupAnagrams(strs);
         lists.forEach(System.out::println);
     }
 
-    public static List<List<String>> groupAnagrams(String[] strs) {
+    public static List<List<String>> groupAnagrams(String[] strList) {
         Map<String, List<String>> map = new HashMap<>();
-        for (String str : strs) {
+        for (String str : strList) {
             char[] array = str.toCharArray();
             // 相同字符的字符串排序后相同
             Arrays.sort(array);
             String key = new String(array);
-            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
             list.add(str);
             map.put(key, list);
         }
