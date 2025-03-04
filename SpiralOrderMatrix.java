4d27307ab9ae80f18e95a180c8dc1225ea8e22f8
x1a0kang <18810298505@163.com>
2025-03-04 22:53:17 +0800

    螺旋矩阵

---
+++ SpiralOrderMatrix.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralOrderMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        List<Integer> res = spiralOrder(matrix);
+        System.out.println(res);
+    }
+
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return res;
+        }
+        int top = 0;
+        int bottom = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+        while (true) {
+            // 从左到右
+            for (int i = left; i <= right; i++) {
+                res.add(matrix[top][i]);
+            }
+            if (++top > bottom) {
+                break;
+            }
+            // 从上到下
+            for (int i = top; i <= bottom; i++) {
+                res.add(matrix[i][right]);
+            }
+            if (--right < left) {
+                break;
+            }
+            // 从右到左
+            for (int i = right; i >= left; i--) {
+                res.add(matrix[bottom][i]);
+            }
+            if (--bottom < top) {
+                break;
+            }
+            // 从上到下
+            for (int i = bottom; i >= top; i--) {
+                res.add(matrix[i][left]);
+            }
+            if (++left > right) {
+                break;
+            }
+        }
+        return res;
+    }
+}
