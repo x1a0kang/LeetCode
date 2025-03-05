64cb6355091d49d7dd7f0b58180a283996694742
x1a0kang <18810298505@163.com>
2025-03-05 20:53:43 +0800

    排序链表

---
+++ SortList.java
@@ -0,0 +1,42 @@
+public class SortList {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(4, new ListNode(2, new ListNode(1, new ListNode(3))));
+        ListNode sorted = sortList(head);
+        while (sorted != null) {
+            System.out.println(sorted.val);
+            sorted = sorted.next;
+        }
+    }
+
+    public static ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode slow = dummy, fast = dummy;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        fast = slow.next;
+        slow.next = null;
+
+        ListNode left = sortList(head);
+        ListNode right = sortList(fast);
+
+        ListNode cur = dummy;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                cur.next = left;
+                left = left.next;
+            } else {
+                cur.next = right;
+                right = right.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = left == null ? right : left;
+        return dummy.next;
+    }
+}
