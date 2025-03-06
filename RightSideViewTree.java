4270d2565215bc44417db99d6600cc1dda93cc12
x1a0kang <18810298505@163.com>
2025-03-06 22:18:20 +0800

    二叉树的右视图

---
+++ RightSideViewTree.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class RightSideViewTree {
+    // 层序遍历，每层取最后一个即可
+    public List<Integer> rightSideView(TreeNode root) {
+        if (root == null) return new ArrayList<Integer>();
+        List<Integer> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        int size;
+        TreeNode temp;
+        while (!queue.isEmpty()) {
+            size = queue.size();
+            for (int i = 0; i < size; i++) {
+                temp = queue.poll();
+                if (temp.left != null) queue.add(temp.left);
+                if (temp.right != null) queue.add(temp.right);
+                if (i == size - 1) {
+                    res.add(temp.val);
+                }
+            }
+        }
+        return res;
+    }
+}
