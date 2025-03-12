4d36b7803022c7cf76277f929cd8e5e97d744219
x1a0kang <18810298505@163.com>
2025-03-12 16:10:11 +0800

    最长递增子序列

---
+++ LengthOfLIS.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class LengthOfLIS {
+    public static void main(String[] args) {
+        System.out.println(lengthOfLIS(new int[]{1, 3, 6, 7, 9, 4, 10, 5, 6}));
+    }
+
+    public static int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int max = dp[0];
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    max = Math.max(max, dp[i]);
+                }
+            }
+        }
+        return max;
+    }
+}
