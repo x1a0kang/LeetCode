51be5e8d9e94e48f89d5c9a03e65205f8d4776fa
x1a0kang <18810298505@163.com>
2025-03-15 20:11:52 +0800

    组合总和

---
+++ CombinationSum.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSum {
+    public static List<List<Integer>> res;
+    public static List<Integer> path;
+
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        res = new ArrayList<>();
+        path = new ArrayList<>();
+        dps(candidates, target, 0);
+        return res;
+    }
+
+    public static void dps(int[] candidates, int target, int start) {
+        if (target <= 0) {
+            if (target == 0) {
+                res.add(new ArrayList<>(path));
+            }
+            return;
+        }
+        for (int i = start; i < candidates.length; i++) {
+            path.add(candidates[i]);
+            dps(candidates, target - candidates[i], i);
+            path.removeLast();
+        }
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> lists = combinationSum(new int[]{2, 3, 6, 7}, 7);
+        for (List<Integer> list : lists) {
+            System.out.println(list);
+        }
+    }
+}
