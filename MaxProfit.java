e7b0860617d8f7499469e62006dc6d61b3ec1474
x1a0kang <18810298505@163.com>
2025-03-17 20:30:31 +0800

    买卖股票的最佳时机

---
+++ MaxProfit.java
@@ -0,0 +1,16 @@
+public class MaxProfit {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0)
+            return 0;
+        int profit = 0;
+        int min = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            } else {
+                profit = Math.max(profit, prices[i] - min);
+            }
+        }
+        return profit;
+    }
+}
