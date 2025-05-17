1ff85580516ce0c10d11e22b4d39efa71ed1e801
x1a0kang <18810298505@163.com>
2025-05-17 15:18:41 +0800

    三数之和

--- ThreeSum.java
+++ ThreeSum.java
@@ -4,25 +4,30 @@ import java.util.List;
 
 public class ThreeSum {
     public static void main(String[] args) {
-        int[] nums = {-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4};
+        int[] nums = {0,0,0,0};
 //        int[] nums = {-1,0,1,2,-1,-4};
-        List<List<Integer>> lists = groupAnagrams(nums);
+        ThreeSum threeSum = new ThreeSum();
+        List<List<Integer>> lists = threeSum.threeSum(nums);
         lists.forEach(System.out::println);
     }
 
-    public static List<List<Integer>> groupAnagrams(int[] nums) {
+    public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
+        // 三数之和要先排序
         Arrays.sort(nums);
         int sum;
         int left, right;
         for (int i = 0; i < nums.length; i++) {
-            if (i - 1 > 0 && nums[i] == nums[i - 1]) {
+            // 如果和前一个数相同，直接跳过，不找重复答案
+            if (i - 1 >= 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
+            // 双指针一左一右
             left = i + 1;
             right = nums.length - 1;
             while (left < right) {
                 sum = nums[left] + nums[right] + nums[i];
+                // 因为是排完序的，如果和大了，就减小右边的值，和小了，就增加左边的值
                 if (sum > 0) {
                     right--;
                 } else if (sum < 0) {
@@ -30,6 +35,7 @@ public class ThreeSum {
                 } else {
                     List<Integer> list = Arrays.asList(nums[i], nums[left], nums[right]);
                     ans.add(list);
+                    // sum等于0后，还要继续循环，因为可能有这种[-3,-2,-2,-1,-1,4,4,5,5]
                     left++;
                     right--;
                     // 跳过重复元素
