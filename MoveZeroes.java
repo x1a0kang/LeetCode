cbf573574e5a918528ec5ad939fa2b8ca5442e35
x1a0kang <18810298505@163.com>
2025-05-17 11:30:53 +0800

    移动零

--- MoveZeroes.java
+++ MoveZeroes.java
@@ -3,23 +3,28 @@ import java.util.Arrays;
 public class MoveZeroes {
     public static void main(String[] args) {
         int[] nums = {0, 1, 0, 3, 12};
-        moveZeroes(nums);
+        MoveZeroes moveZeroes = new MoveZeroes();
+        moveZeroes.moveZeroes(nums);
         System.out.println(Arrays.toString(nums));
     }
 
-    public static void moveZeroes(int[] nums) {
-        // right向右移动，left指在最左边的零，left和right之间的数一定都是零
-        int n = nums.length, left = 0, right = 0;
-        while (right < n) {
-            if (nums[right] != 0) {
-                swap(nums, left, right);
-                left++;
+    // 如果把0往后移，需要向后寻找，时间复杂度更高，可以反过来理解为把非0往前移
+    public void moveZeroes(int[] nums) {
+        // 因为顺序不能改变，用index标识下一个非零元素应该在的位置，cur用来遍历
+        int index = 0;
+        int cur = 0;
+        while (cur < nums.length) {
+            // 如果当前位置不是0，往前交换到index位置，index向后移动一位
+            // 如果当前位置是0，不交换位置，index也不变，遇到下一个不是0的位置，就会交换到这个0的位置来
+            if (nums[cur] != 0) {
+                swap(nums, cur, index);
+                index++;
             }
-            right++;
+            cur++;
         }
     }
 
-    public static void swap(int[] nums, int left, int right) {
+    public void swap(int[] nums, int left, int right) {
         int temp = nums[left];
         nums[left] = nums[right];
         nums[right] = temp;
