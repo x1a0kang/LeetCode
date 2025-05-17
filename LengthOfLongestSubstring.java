3383064ef705f5fc8b69a1b84fc100b35a8f8a93
x1a0kang <18810298505@163.com>
2025-05-17 16:25:02 +0800

    无重复字符的最长子串

--- LengthOfLongestSubstring.java
+++ LengthOfLongestSubstring.java
@@ -3,21 +3,26 @@ import java.util.Map;
 
 public class LengthOfLongestSubstring {
     public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring("u"));
+        LengthOfLongestSubstring l = new LengthOfLongestSubstring();
+        System.out.println(l.lengthOfLongestSubstring("u"));
     }
 
-    public static int lengthOfLongestSubstring(String s) {
+    // 核心思想是滑动窗口
+    public int lengthOfLongestSubstring(String s) {
         if (s == null || s.isEmpty()) return 0;
         char[] charArray = s.toCharArray();
-        int start = 0;
+        int left = 0;
         int max = 0;
+        // map保存key为字符，value为出现的位置
         Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < charArray.length; i++) {
-            if (map.containsKey(charArray[i])) {
-                start = Math.max(map.get(charArray[i]) + 1, start);
+        for (int right = 0; right < charArray.length; right++) {
+            // 如果当前字符在map中存在，并且位置大于left，left向右移动（因为map内的元素不删除，可能有在窗口之外的元素和窗口内的元素重复，这种不用管）
+            if (map.containsKey(charArray[right]) && map.get(charArray[right]) > left) {
+                left = map.get(charArray[right]) + 1;
             }
-            map.put(charArray[i], i);
-            max = Math.max(max, i - start + 1);
+            map.put(charArray[right], right);
+            // 每次扩大窗口，更新max
+            max = Math.max(max, right - left + 1);
         }
         return max;
     }
