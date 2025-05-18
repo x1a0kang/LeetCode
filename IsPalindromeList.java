ca8ee92fee25902d655f91582cf96620d5cd2b19
x1a0kang <18810298505@163.com>
2025-05-18 23:39:20 +0800

    回文链表

--- IsPalindromeList.java
+++ IsPalindromeList.java
@@ -6,12 +6,14 @@ public class IsPalindromeList {
     public boolean isPalindrome(ListNode head) {
         ListNode cur = head;
         List<Integer> list = new ArrayList<>();
+        // 先把链表中的节点都遍历出来
         while (cur != null) {
             list.add(cur.val);
             cur = cur.next;
         }
         int left = 0;
         int right = list.size() - 1;
+        // 然后左右指针判断是否回文
         while (left < right) {
             if (!Objects.equals(list.get(left), list.get(right))) {
                 return false;
@@ -20,5 +22,10 @@ public class IsPalindromeList {
             right--;
         }
         return true;
+    }
+
+    // 空间复杂度是O1的方法时，先找到中点，反转后半部分链表，然后同时遍历前后两半链表
+    public boolean isPalindrome2(ListNode head) {
+        return false;
     }
 }
