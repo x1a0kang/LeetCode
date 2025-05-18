7b87ffbffe7862a402adb817891cd8bf36754714
x1a0kang <18810298505@163.com>
2025-05-18 14:55:24 +0800

    合并区间

--- MergeIntervals.java
+++ MergeIntervals.java
@@ -5,19 +5,22 @@ import java.util.List;
 
 public class MergeIntervals {
     public static void main(String[] args) {
-        int[][] merged = merge(new int[][]{{1, 4}, {4, 5}});
+        MergeIntervals m = new MergeIntervals();
+        int[][] merged = m.merge(new int[][]{{1, 4}, {4, 5}});
         for (int[] i : merged) {
             System.out.println(Arrays.toString(i));
         }
+        System.out.println(Arrays.deepToString(merged));
     }
 
-    public static int[][] merge(int[][] intervals) {
+    public int[][] merge(int[][] intervals) {
         // 先根据每个区间的开头排序
         Arrays.sort(intervals, Comparator.comparing(a -> a[0]));
         List<int[]> res = new ArrayList<>();
         res.add(intervals[0]);
         for (int i = 1; i < intervals.length; i++) {
             int[] interval = intervals[i];
+            // 如果当前数组的左边界小于前一个数组的右边界，合并，否则不重合，直接加入res
             if (interval[0] <= res.getLast()[1]) {
                 res.getLast()[1] = Math.max(res.getLast()[1], interval[1]);
             } else {
