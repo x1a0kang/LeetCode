30e6d1260d7743c4cf6c495b3d688393e8949497
x1a0kang <18810298505@163.com>
2025-05-18 14:42:47 +0800

    买卖股票的最佳时机

--- MaxProfit.java
+++ MaxProfit.java
@@ -1,15 +1,15 @@
 public class MaxProfit {
+    // 核心原理：记录之前的最小值，用当前值减去之前的最小值
     public int maxProfit(int[] prices) {
         if (prices == null || prices.length == 0)
             return 0;
         int profit = 0;
         int min = prices[0];
         for (int i = 1; i < prices.length; i++) {
-            if (prices[i] < min) {
-                min = prices[i];
-            } else {
-                profit = Math.max(profit, prices[i] - min);
-            }
+            // 计算当前位置的最大profit
+            profit = Math.max(profit, prices[i] - min);
+            // 更新最小值
+            min = Math.min(min, prices[i]);
         }
         return profit;
     }
