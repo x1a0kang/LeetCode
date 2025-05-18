4e9160157c5cd8c2012bab0a3fee9201e90e426e
x1a0kang <18810298505@163.com>
2025-05-18 16:21:07 +0800

    除自身以外数组的乘积

--- ProductExceptSelf.java
+++ ProductExceptSelf.java
@@ -6,6 +6,7 @@ public class ProductExceptSelf {
         System.out.println(Arrays.toString(res));
     }
 
+    // 核心是三遍遍历，先求出每个位置左边的乘积，再求出右边的乘积，最后每个位置左右乘积相乘
     public static int[] productExceptSelf(int[] nums) {
         if (nums.length == 0) return new int[0];
         if (nums.length == 1) return nums;
@@ -13,18 +14,22 @@ public class ProductExceptSelf {
         int[] left = new int[nums.length];
         int[] right = new int[nums.length];
 
+        // temp是累计的乘积
         int temp = 1;
+        // 先求左边所有位置的乘积
         for (int i = 0; i < res.length; i++) {
             left[i] = temp;
             temp *= nums[i];
         }
 
         temp = 1;
+        // 再求右边所有位置的乘积
         for (int i = res.length - 1; i >= 0; i--) {
             right[i] = temp;
             temp *= nums[i];
         }
 
+        // 最后左右相乘
         for (int i = 0; i < res.length; i++) {
             res[i] = left[i] * right[i];
         }
