8de83ebfd2f998a8ebbc8c9670a7d4d4b45dca06
x1a0kang <18810298505@163.com>
2025-05-18 23:48:50 +0800

    环形链表 II

--- ListDetectCycle.java
+++ ListDetectCycle.java
@@ -3,6 +3,7 @@ public class ListDetectCycle {
         ListNode slow = head;
         ListNode fast = head;
         boolean isCycle = false;
+        // 先相遇找到环
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
@@ -14,11 +15,12 @@ public class ListDetectCycle {
         if (!isCycle) {
             return null;
         }
-        ListNode detect = head;
-        while (detect != slow) {
-            detect = detect.next;
+        // 这时从头结点出发，慢节点继续前进，相交的点就是环的入口
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
             slow = slow.next;
         }
-        return detect;
+        return fast;
     }
 }
