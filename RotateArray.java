a6e951e1dfa0144b0e6fa121bc017052078fe5fa
x1a0kang <18810298505@163.com>
2025-05-18 15:13:39 +0800

    轮转数组

--- RotateArray.java
+++ RotateArray.java
@@ -2,37 +2,29 @@ import java.util.Arrays;
 
 public class RotateArray {
     public static void main(String[] args) {
+        RotateArray rotateArray = new RotateArray();
         int[] nums = {-1, -100, 3, 99};
-        rotate(nums, 2);
+        rotateArray.rotate(nums, 2);
         System.out.println(Arrays.toString(nums));
     }
 
-    public static void rotate(int[] nums, int k) {
-        // 需要交换的个数
+    public void rotate(int[] nums, int k) {
         int n = nums.length;
-        // 本轮交换的起始位置
-        int start = 0;
-        // 下一个要交换的位置上的数
-        int next = nums[0];
-        // 下一个要交换的位置
-        int nextIndex = 0;
+        // 轮转 k 次等于轮转 k % n 次，因为轮转n次就是不变
+        k %= n;
+        // 先整个数组反转
+        reverse(nums, 0, n - 1);
+        // 然后前k个和后k到n个分别反转
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
 
-        int temp;
-        while (n > 0) {
-            // 当前位置要交换到的目标位置，也是下一个要交换的位置
-            nextIndex = (nextIndex + k) % nums.length;
-            temp = nums[nextIndex];
-            nums[nextIndex] = next;
-            next = temp;
-            // 交换完，n减一
-            n--;
-
-            // 会有一直移动，本轮最后一个元素移动到起始位置上的情况，这时候起始位置前进一位，跳出循环
-            if (nextIndex == start && n > 0) {
-                start++;
-                nextIndex = start;
-                next = nums[nextIndex];
-            }
+    private void reverse(int[] nums, int i, int j) {
+        // 左右指针互换位置
+        while (i < j) {
+            int temp = nums[i];
+            nums[i++] = nums[j];
+            nums[j--] = temp;
         }
     }
 }
