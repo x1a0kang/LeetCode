a944e7ad12efe2653c37a2c0884ef7c6ce4e4234
x1a0kang <18810298505@163.com>
2025-05-18 16:36:32 +0800

    矩阵置零

--- SetZeroes.java
+++ SetZeroes.java
@@ -4,14 +4,16 @@ import java.util.Set;
 
 public class SetZeroes {
     public static void main(String[] args) {
-        int[][] matrix = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
-        setZeroes(matrix);
+        SetZeroes setZeroes = new SetZeroes();
+        int[][] matrix = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+        setZeroes.setZeroes(matrix);
         System.out.println(Arrays.deepToString(matrix));
     }
 
-    public static void setZeroes(int[][] matrix) {
+    public void setZeroes(int[][] matrix) {
         Set<Integer> rows = new HashSet<>();
         Set<Integer> cols = new HashSet<>();
+        // 第一遍遍历记录哪些行和列需要置零
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 if (matrix[i][j] == 0) {
@@ -21,12 +23,14 @@ public class SetZeroes {
             }
         }
 
+        // 所有行置零
         for (Integer row : rows) {
             for (int j = 0; j < matrix[0].length; j++) {
                 matrix[row][j] = 0;
             }
         }
 
+        // 所有列置零
         for (Integer col : cols) {
             for (int j = 0; j < matrix.length; j++) {
                 matrix[j][col] = 0;
