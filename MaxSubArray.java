ce50a5148d91f74c3ec59ea3ff026f5ea5cd7529
x1a0kang <18810298505@163.com>
2025-05-18 14:34:26 +0800

    最大子数组和

--- MaxSubArray.java
+++ MaxSubArray.java
@@ -1,18 +1,24 @@
 public class MaxSubArray {
     public static void main(String[] args) {
-        int[] nums = {-1};
-        System.out.println(maxSubArray(nums));
+        MaxSubArray maxSubArray = new MaxSubArray();
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(maxSubArray.maxSubArray(nums));
     }
 
-    public static int maxSubArray(int[] nums) {
+    // 核心原理：前缀和，前缀和的关键是，需要的子数组，是当前前缀和减去之前前缀和的差值
+    public int maxSubArray(int[] nums) {
+        // 当前前缀和
         int preSum = 0;
+        // 之前的最小前缀和
+        int minPreSum = 0;
         int max = nums[0];
         for (int num : nums) {
-            if (preSum < 0) {
-                preSum = 0;
-            }
+            // 计算当前前缀和
             preSum += num;
-            max = Math.max(max, preSum);
+            // 当前前缀和减去之前的最小前缀和，计算以当前位置为结束的子数组的最大和
+            max = Math.max(max, preSum - minPreSum);
+            // 更新最小前缀和
+            minPreSum = Math.min(preSum, minPreSum);
         }
         return max;
     }
