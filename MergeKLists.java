05204583b1550dfdcf3f0f48d49091a1b0b633c0
x1a0kang <18810298505@163.com>
2025-05-19 00:29:36 +0800

    合并 K 个升序链表

--- MergeKLists.java
+++ MergeKLists.java
@@ -5,7 +5,9 @@ public class MergeKLists {
     // 也可以分治，两两合并，递归
     public ListNode mergeKLists(ListNode[] lists) {
         if (lists == null || lists.length == 0) return null;
+        // 维护一个最小堆
         PriorityQueue<ListNode> pq = new PriorityQueue<>(lists.length, Comparator.comparingInt(o -> o.val));
+        // 先把所有头节点都放进去
         for (ListNode node : lists) {
             if (node != null) {
                 pq.add(node);
@@ -15,6 +17,7 @@ public class MergeKLists {
         ListNode head = new ListNode(0);
         ListNode cur = head;
         ListNode temp;
+        // 一直弹出堆顶元素，并加入弹出元素的next
         while (!pq.isEmpty()) {
             temp = pq.poll();
             cur.next = temp;
