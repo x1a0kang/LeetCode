748670f60a29cb555cfd303b802e2701ca25182f
x1a0kang <18810298505@163.com>
2025-05-19 10:53:41 +0800

    从前序与中序遍历序列构造二叉树

--- BuildTree.java
+++ BuildTree.java
@@ -7,18 +7,21 @@ public class BuildTree {
     private static Map<Integer, Integer> map;
 
     public static void main(String[] args) {
+        BuildTree buildTree = new BuildTree();
         LevelOrderTree levelOrderTree = new LevelOrderTree();
         int[] preorder = {3, 9, 20, 15, 7};
         int[] inorder = {9, 3, 15, 20, 7};
-        TreeNode root = buildTree(preorder, inorder);
+        TreeNode root = buildTree.buildTree(preorder, inorder);
         List<List<Integer>> order = levelOrderTree.levelOrder(root);
         System.out.println(Arrays.toString(order.toArray()));
     }
 
-    public static TreeNode buildTree(int[] preorder, int[] inorder) {
+    // 核心原理就是找到根节点，从中序中找到左右子树的范围，然后递归
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
         if (preorder == null || preorder.length == 0 || inorder == null || inorder.length == 0) {
             return null;
         }
+        // 记录中序数组中所有数字的下标，方便后续直接定位
         map = new HashMap<>();
         for (int i = 0; i < inorder.length; i++) {
             map.put(inorder[i], i);
@@ -26,12 +29,14 @@ public class BuildTree {
         return buildTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
     }
 
-    public static TreeNode buildTree(int[] preorder, int pStart, int pEnd, int[] inorder, int iStart, int iEnd) {
+    public TreeNode buildTree(int[] preorder, int pStart, int pEnd, int[] inorder, int iStart, int iEnd) {
         if (pStart > pEnd || iStart > iEnd || pStart > preorder.length - 1 || iStart > inorder.length - 1) {
             return null;
         }
+        // 前序第一个节点是根节点
         int rootVal = preorder[pStart];
         TreeNode root = new TreeNode(rootVal);
+        // 从中序中找到根节点的下标，左边是左子树，右边是右子树
         int index = map.get(rootVal);
         if (index != -1) {
             root.left = buildTree(preorder, pStart + 1, pStart + (index - iStart), inorder, iStart, index - 1);
