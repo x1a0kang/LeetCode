4a2febd6c12c8fa3db36ec0b7ed76977e441129f
x1a0kang <18810298505@163.com>
2025-05-19 13:03:02 +0800

    单词拆分

--- WordBreak.java
+++ WordBreak.java
@@ -1,20 +1,27 @@
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 public class WordBreak {
+    public static void main(String[] args) {
+        String s = "aaaaaaa";
+        List<String> wordDict = new ArrayList<>();
+        wordDict.add("aaaa");
+        wordDict.add("aaa");
+        WordBreak wordBreak = new WordBreak();
+        System.out.println(wordBreak.wordBreak(s, wordDict));
+    }
+
     public boolean wordBreak(String s, List<String> wordDict) {
         if (s == null || wordDict == null) {
             return false;
         }
-        Set<String> wordDictSet = new HashSet<>(wordDict);
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
-        for (int i = 1; i <= s.length(); i++) {
-            for (int j = 0; j < i; j++) {
-                if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
-                    dp[i] = true;
-                    break;
+        for (int i = 0; i < s.length(); i++) {
+            // 以i为起点，判断从i到j是否是可选字符串，取两者的与
+            for (int j = i + 1; j <= s.length(); j++) {
+                if (dp[i] && wordDict.contains(s.substring(i, j))) {
+                    dp[j] = true;
                 }
             }
         }
