91e52caf0154152ee7535f779e4c604f592c31d3
x1a0kang <18810298505@163.com>
2025-05-19 13:11:52 +0800

    最长递增子序列

--- LengthOfLIS.java
+++ LengthOfLIS.java
@@ -2,15 +2,17 @@ import java.util.Arrays;
 
 public class LengthOfLIS {
     public static void main(String[] args) {
-        System.out.println(lengthOfLIS(new int[]{1, 3, 6, 7, 9, 4, 10, 5, 6}));
+        LengthOfLIS lengthOfLIS = new LengthOfLIS();
+        System.out.println(lengthOfLIS.lengthOfLIS(new int[]{1, 3, 6, 7, 9, 4, 10, 5, 6}));
     }
 
-    public static int lengthOfLIS(int[] nums) {
+    public int lengthOfLIS(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
         int[] dp = new int[nums.length];
         Arrays.fill(dp, 1);
         int max = dp[0];
         for (int i = 1; i < nums.length; i++) {
+            // 每个i，遍历它之前的数，如果比当前位置小，那就可以和当前位置组成一个递增序列，不一定要连续
             for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
