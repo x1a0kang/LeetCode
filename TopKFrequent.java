996d714ed80afd033cf963f6bee2cc075966af60
x1a0kang <18810298505@163.com>
2025-05-21 20:51:56 +0800

    前 K 个高频元素

--- TopKFrequent.java
+++ TopKFrequent.java
@@ -1,26 +1,27 @@
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class TopKFrequent {
     public static void main(String[] args) {
-        topKFrequent(new int[]{1, 1, 1, 2, 2, 3}, 2);
+        TopKFrequent topKFrequent = new TopKFrequent();
+        System.out.println(Arrays.toString(topKFrequent.topKFrequent(new int[]{1, 1, 1, 2, 2, 3}, 2)));
     }
 
-    public static int[] topKFrequent(int[] nums, int k) {
+    public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
+        // 用map计数
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
+        // 用最小堆进行排序
+//        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
+        // Comparator的另一种方式，实现最大堆
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((o1, o2) -> o2.getValue() - o1.getValue());
         pq.addAll(map.entrySet());
-        while (pq.size() > k) {
-            pq.poll();
-        }
+        // 弹出最大堆顶k个元素
+        int index = 0;
         int[] res = new int[k];
-        while (!pq.isEmpty()) {
-            res[pq.size() - 1] = pq.poll().getKey();
+        while (index < k) {
+            res[index++] = pq.poll().getKey();
         }
         return res;
     }
