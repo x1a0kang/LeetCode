d1439fa583188dc08d4fcabb8a8c6df406f63a18
x1a0kang <18810298505@163.com>
2025-05-29 12:07:08 +0800

    单词拆分

--- WordBreak.java
+++ WordBreak.java
@@ -18,9 +18,13 @@ public class WordBreak {
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
         for (int i = 0; i < s.length(); i++) {
+            // 如果i不是true，直接跳过
+            if (!dp[i]) {
+                continue;
+            }
             // 以i为起点，判断从i到j是否是可选字符串，取两者的与
             for (int j = i + 1; j <= s.length(); j++) {
-                if (dp[i] && wordDict.contains(s.substring(i, j))) {
+                if (wordDict.contains(s.substring(i, j))) {
                     dp[j] = true;
                 }
             }
