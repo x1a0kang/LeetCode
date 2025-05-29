a5e0c7a705aaafd5cbac18c64a580d55ba0b5d68
x1a0kang <18810298505@163.com>
2025-05-29 10:17:20 +0800

    最长连续序列

--- LongestConsecutive.java
+++ LongestConsecutive.java
@@ -2,6 +2,11 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class LongestConsecutive {
+    /**
+     * 寻找数组中最长的连续数字序列长度
+     * @param nums 输入的整数数组
+     * @return 最长连续数字序列的长度
+     */
     public int longestConsecutive(int[] nums) {
         Set<Integer> set = new HashSet<>();
         for (int num : nums) {
@@ -9,11 +14,17 @@ public class LongestConsecutive {
         }
         int res = 0;
         int temp;
-        // 这里一定要遍历set，如果原数组是[1,1,1,2,3,5]，会遍历三遍1
+        /*
+         * 这里一定要遍历set，如果原数组是[1,1,1,2,3,5]，会遍历三遍1
+         * 遍历set时，如果当前数字前一个不存在（即num-1不在集合中），则开始计算连续序列
+         * 然后不断递增temp直到在集合中找不到对应的值，计算连续序列的长度
+         */
         for (int num : set) {
+            // 如果前一个数存在，跳过，因为前一个已经算过了
             if (set.contains(num - 1)) {
                 continue;
             }
+            // 如果前一个不存在，开始往后找0
             temp = num + 1;
             while (set.contains(temp)) {
                 temp++;
