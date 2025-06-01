34695b23400d54c84899030afd99140700bad0d6
2x1a0kang <46711942+2x1a0kang@users.noreply.github.com>
2025-06-01 21:40:13 +0800

    分割等和子集

--- CanPartition.java
+++ CanPartition.java
@@ -2,10 +2,11 @@ import java.util.Arrays;
 
 public class CanPartition {
     public static void main(String[] args) {
-        canPartition(new int[]{100, 4, 6});
+        CanPartition canPartition = new CanPartition();
+        System.out.println(canPartition.canPartition(new int[]{100, 4, 6}));
     }
 
-    public static boolean canPartition(int[] nums) {
+    public boolean canPartition(int[] nums) {
         int sum = 0;
         for (int num : nums) {
             sum += num;
@@ -20,17 +21,20 @@ public class CanPartition {
             dp[0][nums[0]] = true;
         }
         for (int i = 1; i < nums.length; i++) {
+            // 每行的第一列都是true，因为目标是0
             dp[i][0] = true;
             for (int j = 1; j <= target; j++) {
+                // 如果当前数大于目标，肯定不选当前数，dp[i][j]等于上一行同一列dp[i - 1][j]
                 if (nums[i] > j) {
                     dp[i][j] = dp[i - 1][j];
-                } else {
+                }
+                // 如果当前数大于目标，可以选当前数，
+                // 如果不选，dp[i][j]等于上一行同一列dp[i - 1][j]
+                // 如果选，目标数减去num[i]就是剩的数，看dp中上一行，剩的数的列是否是true
+                else {
                     dp[i][j] = dp[i - 1][j - nums[i]] || dp[i - 1][j];
                 }
             }
-        }
-        for (int i = 0; i < nums.length; i++) {
-            System.out.println(Arrays.toString(dp[i]));
         }
         return dp[nums.length - 1][target];
     }
