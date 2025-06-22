8985c14f526e00e544dd404ebd74b1b408835ff3
x1a0kang <18810298505@163.com>
2025-06-22 19:00:43 +0800

    电话号码是字母组合

--- LetterCombinations.java
+++ LetterCombinations.java
@@ -1,38 +1,50 @@
 import java.util.*;
 
 public class LetterCombinations {
-    public static List<String> res;
-    public static StringBuilder sb;
+    private List<String> res;
+    private StringBuilder sb;
 
-    public static List<String> letterCombinations(String digits) {
+    public static void main(String[] args) {
+        LetterCombinations letterCombinations = new LetterCombinations();
+        List<String> list = letterCombinations.letterCombinations("23");
+        System.out.println(list);
+    }
+
+    public List<String> letterCombinations(String digits) {
         if (digits == null || digits.isEmpty()) {
             return new ArrayList<>();
         }
         res = new ArrayList<>();
         sb = new StringBuilder();
-        Map<Character, List<String>> letterMap = getLetterMap();
         List<List<String>> letterList = new ArrayList<>();
+        // 获取每个数字的待选列表
         for (int i = 0; i < digits.length(); i++) {
-            letterList.add(letterMap.get(digits.charAt(i)));
+            letterList.add(getLetterList(digits.charAt(i)));
         }
         dfs(letterList, 0);
         return res;
     }
 
-    public static void dfs(List<List<String>> letterList, int num) {
+    // 每个数字的待选列表内只能选一个，num表示该选第几个数字的待选列表
+    public void dfs(List<List<String>> letterList, int num) {
+        // 已选的字符数和输入的字符数相同时，说明已选完所有字符，加入结果集
         if (sb.length() == letterList.size()) {
             res.add(sb.toString());
             return;
         }
+        // 遍历当前待选列表
         List<String> list = letterList.get(num);
         for (String s : list) {
+            // 添加当前字符
             sb.append(s);
+            // 递归
             dfs(letterList, num + 1);
+            // 删除当前字符，最后一个字符
             sb.deleteCharAt(sb.length() - 1);
         }
     }
 
-    public static Map<Character, List<String>> getLetterMap() {
+    public List<String> getLetterList(Character c) {
         Map<Character, List<String>> map = new HashMap<>();
         map.put('2', Arrays.asList("a", "b", "c"));
         map.put('3', Arrays.asList("d", "e", "f"));
@@ -42,11 +54,6 @@ public class LetterCombinations {
         map.put('7', Arrays.asList("p", "q", "r", "s"));
         map.put('8', Arrays.asList("t", "u", "v"));
         map.put('9', Arrays.asList("w", "x", "y", "z"));
-        return map;
-    }
-
-    public static void main(String[] args) {
-        List<String> list = letterCombinations("23");
-        System.out.println(list);
+        return map.get(c);
     }
 }
