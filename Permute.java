450920db37e3caa5d68726d1d141321b9c44ed84
x1a0kang <18810298505@163.com>
2025-06-22 17:50:02 +0800

    全排列

--- Permute.java
+++ Permute.java
@@ -3,18 +3,20 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Permute {
-    public static void main(String[] args) {
-        List<List<Integer>> permute = permute(new int[]{1, 2, 3});
-        System.out.println(Arrays.deepToString(permute.toArray()));
-    }
-
     public static List<List<Integer>> res;
 
-    public static List<List<Integer>> permute(int[] nums) {
+    public static void main(String[] args) {
+        Permute permute = new Permute();
+        List<List<Integer>> result = permute.permute(new int[]{1, 2, 3});
+        System.out.println(Arrays.deepToString(result.toArray()));
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
         if (nums == null || nums.length == 0) {
             return new ArrayList<>();
         }
         res = new ArrayList<>();
+        // []不适合后续操作，先转移到ArrayList中
         List<Integer> arr = new ArrayList<>();
         for (int num : nums) {
             arr.add(num);
@@ -23,16 +25,24 @@ public class Permute {
         return res;
     }
 
-    public static void dfs(List<Integer> arr, List<Integer> list) {
+    public void dfs(List<Integer> arr, List<Integer> list) {
+        // 当数组内没有数据了，说明一个排列完成
         if (arr.isEmpty()) {
             res.add(new ArrayList<>(list));
             return;
         }
+        // 记录本轮被选择的元素，删除后递归，再添加
+        Integer removed;
         for (int i = 0; i < arr.size(); i++) {
+            // 加入排列
             list.add(arr.get(i));
-            arr.remove(i);
+            // 删除当前元素
+            removed = arr.remove(i);
+            // 递归
             dfs(arr, list);
-            arr.add(i, list.getLast());
+            // 把删除的元素添加回原数组
+            arr.add(i, removed);
+            // 排列中当前元素
             list.removeLast();
         }
     }
